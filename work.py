21ce580684f7a68387adf97969afeaf35bc27030
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2023-09-08 12:35:57 +0100

    Add files via upload

---
+++ work.py
@@ -0,0 +1,17 @@
+mars_bar = 1
+can_of_coke = 1.50
+bag_of_crisps = 80
+cup_of_tea = 2
+slice_of_pan = 3.50
+
+mars_bar = print("The quantity for a mars bar is 5x, for €1")
+can_of_coke = print("The quantity for a can of coke is 4x, for  €1.50")
+bag_of_crisps = print("The quantity for a bag of crisp is 3x, for 80c")
+cup_of_tea = print("The quantity for a cup of tea is 2x, for  €2")
+slice_of_pan = print("The quantity for a slice of pan is 1x, for  €3.50")
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+sum = number1 + number2
+print("The answer is sum")
+print(number1, "+", number2, "=", sum)
