5f02000cef52eec6c5d0cff450087ef0c3b448cb
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2023-09-22 12:38:15 +0100

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,9 @@
+listofsports = ["Football","Basketball"]
+favorite = input("What is your favorite sport?")
+listofsports.append(favorite)
+print(listofsports)
+
+subjects = ["Maths","English","Science","histroy","Geography","Cspe"]
+notfavsubject = input("Which subject dont you like?:")
+subjects.remove(notfavsubject)
+print("here is the new list",subject)
