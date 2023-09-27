f957924d31cd66f3efea76007eda43bf9212437c
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2023-09-27 13:12:19 +0100

    Add files via upload

--- loop.py
+++ loop.py
@@ -3,7 +3,13 @@ favorite = input("What is your favorite sport?")
 listofsports.append(favorite)
 print(listofsports)
 
-subjects = ["Maths","English","Science","histroy","Geography","Cspe"]
+subjects = ["Maths","English","Science","history","Geography","Cspe"]
 notfavsubject = input("Which subject dont you like?:")
 subjects.remove(notfavsubject)
-print("here is the new list",subject)
+print("here is the new list",subjects)
+
+TVshow = ["family guy","breaking bad","the simpson","friends","stranger things"]
+for variable in list:
+    tvshowname = input("add another tv show name:")
+    print(tvshowname)
+    varibale = "
