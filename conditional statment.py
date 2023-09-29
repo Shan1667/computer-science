4b680616141b3478cba7a13d237b9c3457161858
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2023-09-29 12:33:08 +0100

    Add files via upload

---
+++ conditional statment.py
@@ -0,0 +1,17 @@
+
+numbers = input("please enter a number that is under 20:")
+numbers = int(numbers)
+if numbers < 20:
+                print("Thank You!")
+else:
+                print("Too High!")
+                
+num = input("please enter a number between 10 and 20:")
+num = int(num)
+if num  <=20 and num >= 10:
+    print("Thank you")
+else:
+    print("Incorrect answer")
+    
+colour = input("what is your favorite colour:")
+if 
