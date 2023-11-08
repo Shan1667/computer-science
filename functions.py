6f3435e82eb27bbcdd1b2a4c71b568e5ff69ce5c
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2023-11-08 11:59:10 +0000

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,14 @@
+
+def someNum():
+    x = input("enter a number")
+    x = int(x)
+    return x
+num = someNum()
+
+def myCount(value):
+    for i in range(1,value+1):
+        print(i)
+myCount(num)
+    
+def somenum(num):
+    x = input
