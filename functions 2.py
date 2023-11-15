647d844f0ca39a5c729cb22889991a09c91b402c
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2023-11-15 13:10:37 +0000

    Add files via upload

---
+++ functions 2.py
@@ -0,0 +1,19 @@
+A = int()
+B = int()
+C = int()
+print("we will try to find the greatest of these three numbers")
+print("enter three integers inputs:")
+if A > B and A > C:
+    print("A is the greatest number")
+elif C > A and C > B:
+    print("C is the greatest number")
+else:
+    print("B is the greatest number")
+    
+print("please enter three integers input")
+if A > B and A > C:
+    print("A is the greatest")
+elif B > C:
+    print("B is the greatest")
+else:
+    print("C is the greatest")
