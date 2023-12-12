f712a8a2b24afacf5b0345b6d3e6f89e784959fc
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2023-12-12 21:55:41 +0000

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,35 @@
+number = input("enter 6 numbers")
+list = [num for num in str(number)]
+name = [num for num in list if list.index(num) % 2 == 0]
+print(name)
+
+name = []
+list = int(input("enter "))
+
+print('Enter',str(size),'positive numbers')
+
+for i in range(list):
+    numbs = int(input())
+    name.append(numbs)
+
+score = 0
+for data in name:
+    if numbs > score:
+        score = numbs
+
+print('The largest number in list is', score)
+
+
+list = int(input("enter 5 numbers:"))
+for i in range(list):
+    name = int(list)
+    list.append(data)
+    
+print("here are the numbers")
+
+for i in range(0,size,2):
+    print(lists[i])
+    
+    
+name = input("please enter your name?")
+print(name)
