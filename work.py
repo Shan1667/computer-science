965953244af5e3dd9f36d879e02af08d716765a0
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2024-01-08 11:56:03 +0000

    Add files via upload

--- work.py
+++ work.py
@@ -1,25 +1,5 @@
-name = input("please enter your name:")
-print("hello, your name is", name,)
-
-numb = input("please enter two number")
-numbs = input("lpease enter another number")
-print("the answer is", numb + numbs,)
-
-temperature = int(input("Enter the Temperature in Celsius :\n"))
-temperature2 = (9.5 * temperature) + 32.
-print("Temperature in Fahrenheit :", temperature2)
-
-def intrest(numbs,numb,number):
-    print('The principal is', numbs)
-    print('The time period is', numb)
-    print('The rate of interest is',number)
-     
-    si = (numbs * numb* number)/100
-     
-    print('The Simple Interest is', si)
-     
-numbs = int(input("Enter the principal amount :"))
-numb = int(input("Enter the time period :"))
-number = int(input("Enter the rate of interest :"))
-intrest(numbs,numb,number)
- 
+excel = open("remainder.txt", "r")
+#excel.read()
+for line in excel:
+    print(line)
+excel.close()
