e420e891f1c275903099b4a45c01e0b34d01a815
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2024-02-07 13:12:39 +0000

    Add files via upload

---
+++ decimal 2.py
@@ -0,0 +1,13 @@
+name = input("please press 1 if want to convert a binary number to a decimal number or press 2 to enter a decimal number to a binary number:")
+#if "1" in name:
+
+   # names = input("please enter a 5 digit binary number:")
+if "2"in name:
+    total = 0
+    numb = input("please enter a 5 digit decimal number:")
+    numbs = len(numb)-1
+    for digit in numb:
+        total = total+ int(digit) * 2** numbs
+        numbs = numbs -1 
+#for number in names:
+print(total)
