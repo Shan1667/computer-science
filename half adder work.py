8400d06dd7f4cbaffc6d6e116232dff487c298d8
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2024-03-01 12:35:12 +0000

    Add files via upload

---
+++ half adder work.py
@@ -0,0 +1,57 @@
+def and_gate (a, b):
+
+    if a == 1 and b == 1:
+      return 1
+    else:
+      return 0
+    
+print(and_gate(1,0))
+
+
+def not_gate (a, b):
+    
+    if a == 1 and b == 1:
+      return 1
+    else:
+      return 0
+    
+print(not_gate(1,0))
+
+
+def or_gate (a, b):
+
+    if a == 1 and b == 1:
+      return 1
+    else:
+      return 0
+    
+print(or_gate(1,0))
+        
+        
+def xor_gate (a, b):
+    
+    if a == 1 and b == 1:
+      return 1
+    else:
+     return 0
+    
+print(xor_gate(1,0))
+
+
+def nand_gate (a, b):
+
+    if a == 1 and b == 1:
+      return 1
+    else:
+      return 0
+    
+print(nand_gate(1,0))
+
+
+def nor_gate (a, b):
+    if a == 1 and b == 1:
+      return 1
+    else:
+      return 0
+    
+print(nor_gate(1,0))
