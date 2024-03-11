0dac1d5db840ecde8235b1cf161d9c17e982885e
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2024-03-11 11:55:03 +0000

    Add files via upload

---
+++ function exercise 12.py
@@ -0,0 +1,38 @@
+add1 = input("enter a number:")
+add1 =int(add1)
+add2 = input("please enter a second number:")
+add2 =int(add2)
+
+def name(a,b):
+  totals = a + b
+  return totals
+
+num =name(add1,add2)
+print(num)
+
+
+word = input("please enter a word:")
+un = "un"
+def names(c,d):
+  total1 = c + d
+  return total1
+
+numbs = names(un,word)
+print(numbs)
+
+
+words = input("please enter a palindrome word that means the same fowards and backwards:")
+
+def string(A):
+    b=A[::-1]
+    return b
+
+num = string(words)
+if num==words:
+    print("it is a palindrome")
+else:
+    print("it is not a palindrome")
+    
+    
+lists = input("please enter 10 numbers:")
+while "lists"
