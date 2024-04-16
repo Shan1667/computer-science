ca313a86ab70b876d344d6f973ef398577db098e
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2024-04-16 21:14:18 +0100

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,16 @@
+name = input("please enter the value of NUM1:")
+names = input("please enter the value of NUM2:")
+if "NUM1" < "NUM2" :
+    print("smallest is NUM1")
+else:
+    print("smallest is NUM2")
+    ENDIF
+    
+    
+    
+numb = input("please enter the value of num1:")
+numbs = input("please enter the value of num2:")
+if "num1" > "num2" :
+    print("biggest is num1")
+else:
+    print("biggest is num2")
