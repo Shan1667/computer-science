5c4de0018922a8f9449a1db7712aaccc6794f77e
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2024-04-16 21:14:45 +0100

    Add files via upload

---
+++ tax.py
@@ -0,0 +1,8 @@
+account = input("please enter the principle amount:")
+intrest = input("please enter the annual rate of intrest:")
+int(intrest)
+int(account)
+year = input("please enter the time period:")
+int(year)
+rate = intrest / 100
+numb = account * rate * year
