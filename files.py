6b553dc7b169f1ef97be178e32f998f1a80c269d
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2024-09-11 14:40:40 +0100

    Add files via upload

---
+++ files.py
@@ -0,0 +1,24 @@
+file = open("C:\\Users\\20SShahid.ACC\\Documents\\Exercise.csv", "r")
+name = file.readline()
+print(name)
+
+lists = []
+for line in file:
+    line = line.strip()
+    line = line.split(",")
+    lists.append(line)
+_60minutesessions = 0.0
+for item in lists:
+    if(item[0] == 60):
+        _60minutesessions += item[3]
+       
+    print(f'{item} --- {item[0]}')
+for i in lists:
+    print(mniesnf)
+    _60minutesessions = 0
+    for i in lists:
+        _60minutesessions + _60minutesessions + i
+    _60minutesessions = /len(lists)
+    
+    
+
