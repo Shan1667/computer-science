21e4cb8c2ca39deeb3fba1723fbdff4c444a38ae
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2024-10-03 15:38:58 +0100

    Add files via upload

--- works.py
+++ works.py
@@ -6,19 +6,58 @@ print(counts2)
 menu = input("please enter frequency to see how many words come up or enter characters to see how many characters come up:")
 if "frequency" in menu:
     sentence = input("please enter a sentence:")
+    sentence = sentence.upper()
+    print(sentence)
+    words = sentence.split()
+    print(words)
+    for item in words:
+        
+        if item in counts:
+            
+            counts[item] = counts[item] + 1
+        else:
+            
+             counts[item] = 1
+    print(counts)
 
-elif "characters" in menu:
-    print("HI")
+if "characters" in menu:
+    letters = input("please enter a sentence to calculate each characters:")
+    letters = letters.upper()
+    print(letters)
+    for item in letters:
+        
+        if item in counts:
+            
+            counts[item] = counts[item] + 1
+        else:
+            
+             counts[item] = 1
+    print(counts)
+  
 
-sentence = sentence.upper()
-print(sentence)
-words = sentence.split()
-print(words)
-for item in words:
-    if item in counts:
-        counts[item] = counts[item] + 1
+book = open("book.txt","r")
+x = book.read()
+print(x)
+
+names = x.split()
+print(names)
+for number in names:
+    if number in counts:
+        counts[number] = counts[number] + 1
     else:
-        counts[item] = 1
+        counts[number] = 1
 print(counts)
+
+
+#sentence = sentence.upper()
+#print(sentence)
+#words = sentence.split()
+#print(words)
+#for item in words:
+    #if item in counts:
+        #counts[item] = counts[item] + 1
+   # else:
+        #counts[item] = 1
+#print(counts)
 
 
