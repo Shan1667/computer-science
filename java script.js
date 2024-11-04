100c45a043c570d4b452eb1e14ba93db9eb93767
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2024-11-04 12:00:16 +0000

    Add files via upload

---
+++ java script.js
@@ -0,0 +1,37 @@
+console.log("hello world");
+//console are ignored by the computer
+//two fowards slashes are single
+/*
+this is a
+multi_line comment
+*/
+//you can store data in variables
+//to declare a variable use the let keyword
+let myvariable = 5;
+console.log(myvariable);
+//text is written in quotes
+let myname = "John";
+let myothername = "Doe";
+console.log(myname,myothername);
+//variables are case sensitive
+let myName = "Bob"; //this is diffrent to myname
+console.log(myname,myName);
+//variables must start with a letter [a-zA-Z]
+//underscore _ or dollar sign $
+$some_varibles = 200;
+_someothervriables = 500
+total = $some_varibles + _someothervriables;
+console.log(total);
+//maths operator are like ordianry maths
+//+ add
+// - subtract
+// / divison
+// % reminder
+ let addtion = 4+4;
+ let subtraction = 5-4
+ let multiply = 4*3;
+ let divide = 4/2
+ // reminder gives the remainder after division
+ let reinder = 6%4;
+ //the exponent or power is **
+ let four_cubed = 4**3
