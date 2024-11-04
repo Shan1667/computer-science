807c002c574a5d57f091e939d334648fabdfc538
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2024-11-04 12:00:00 +0000

    Add files via upload

---
+++ java script work.js
@@ -0,0 +1,21 @@
+let numbers  = 7;
+let numbers2 = 9.5;
+let numbers3 = 10
+console.log(numbers,numbers2,numbers3);
+let newNumber = 7+9.5;
+let newNumber2 = 9.5+10;
+console.log(newNumber,newNumber2);
+let oldNumber = 7;
+let newnumber = +1;
+let newnumber2 = +2;
+console.log(oldNumber,oldNumber+1,oldNumber+2);
+let radius = 10;
+let Pi = 3.14;
+let answer_to_Pi = (Pi)*(radius);
+console.log(answer_to_Pi)
+let base = 5;
+let height = 10;
+let middle = 1/2
+let area = (base*height)*(middle);
+console.log(area)
+
