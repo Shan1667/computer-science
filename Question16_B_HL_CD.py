260b309ad54e12dcc541b131f70bd3b1b9ce2ce5
Shan1667 <144443986+Shan1667@users.noreply.github.com>
2025-05-02 12:59:06 +0100

    Add files via upload

---
+++ Question16_B_HL_CD.py
@@ -0,0 +1,32 @@
+#Question_16_B_HL
+#Enter your name here:
+
+import random
+print("Welcome to my shop")
+total = 0
+item_list = []
+item_prices = []
+item_list_input = input("please enter the item:")
+item_prices_input = input("please enter the price of the item:")
+#lists = item_list_input.append()
+lists2 = item_prices_input.append()
+while item_list_input in item_list_input and item_prices_input in item_prices_input:
+    item_list_input = input("please enter the item:")
+    item_prices_input = input("please enter the price of the item:")
+if "stop" in item_list_input:
+    item_list_input("your item list is:")
+    #print("the total is", item_prices + total)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
